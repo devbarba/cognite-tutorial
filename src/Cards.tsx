25aca0e8ae1c9b20ad304c77530f4a5fd5ba6117
Minion <harishkrishnan@Harish.local>
2021-11-02 14:17:14 +0000

    styling

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 import Datas from '../src/assets/data.json'
-import CogniteCard from './assets/styles'
+import {CogniteCard, CogniteContent} from './assets/styles'
 
 export default function BasicCard() {
   return (
@@ -14,21 +11,17 @@ export default function BasicCard() {
       {Datas.devteam.map((data, index) => (
         <Grid item xs={12} sm={6} md={3} key={index}>
          <CogniteCard>
-          <Card>
-            <CardContent>
-              <Typography variant="h5" component="h2">
+          <Paper style={{borderRadius : "20px"}}>
+          <CogniteContent>
+          <Typography variant="h5" component="h2">
                 {data.name}
               </Typography>
               <Typography variant="body2" component="p">
-                {data.department}
+                {data.country}
               </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary">
-              {data.country}
-              </Button>
-            </CardActions>
-          </Card>
+            </CogniteContent>
+          </Paper>
+        
           </CogniteCard>
         </Grid>
       ))}
--- src/assets/styles.tsx
+++ src/assets/styles.tsx
@@ -1,7 +1,31 @@
 import styled from 'styled-components'
 
-const CogniteCard = styled.div`
+export const CogniteCard = styled.div`
     margin-top : 10vh;
     margin-bottom : 2vh;
+    &:hover{
+     
+    }
+  
   `
-export default CogniteCard
+export const CogniteContent = styled.div`
+   padding :8vh;
+   &:hover {
+    border-radius : 20px;
+    background: linear-gradient(to right bottom, rgb(0, 127, 255), rgb(0, 89, 178) 120%);
+    color : white;
+    font-style : italic;
+    //background : lightblue;
+    cursor : pointer;
+  }
+ 
+  `
+export const AssetTable = styled.div`
+    height : 70vh;
+    padding : 2vh 5vh 1vh 5vh;
+    `
+
+
+
+
+
